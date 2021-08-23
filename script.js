28a61f695d3b1b8292777ee3ceb8c610d3f0eea8
Islam <sagov-2001@mail.ru>
2021-08-23 17:12:33 +0300

    Теперь можно выбирать язык и тему.

---
+++ script.js
@@ -0,0 +1,61 @@
+"use strict"
+
+const langBtn = document.querySelector('.header__select-lang-btn'),
+      langList = document.querySelector('.header__lang-list'),
+      svgArrow = document.querySelector('.svg-arrow'),
+      btnSun = document.querySelector('.dark-theme'),
+      btnMoon = document.querySelector('.white-theme'),
+      head = document.querySelector('head');
+
+
+// ---------------------------------------------------------------------------
+
+
+function selectLang() {
+    langBtn.addEventListener('click', () => {
+        langList.classList.toggle('hide');
+        langBtn.classList.toggle('shadow');
+        svgArrow.classList.toggle('rotate');
+    });
+    
+    langList.addEventListener('click', (event) => {
+        if (event.target.id == 'eng') {
+            location.href = 'eng-version.html';
+
+        } else if (event.target.id == 'ru') {
+            location.href = 'index.html'
+        }
+    });
+
+
+    function closeLangListWhenClickWindow () {
+        window.addEventListener('click', (e) => {
+            const event = e.target;
+            if (event.id !== 'select-lang' && event.id !== 'lang-span' && event.id !== 'svg-arrow') {
+                langList.classList.add('hide');
+                svgArrow.classList.remove('rotate')
+            }
+        });
+    }
+    closeLangListWhenClickWindow()
+
+}
+selectLang();
+
+
+function selectThem() {
+    function removeAndAdd(remove, add) {
+        remove.classList.remove('hide')
+        add.classList.add('hide')
+    }
+    btnMoon.addEventListener('click', () => {
+        removeAndAdd(btnSun,btnMoon)
+        head.innerHTML += '<link id="dark-theme" rel="stylesheet" href="css/dark-mode.css">'
+    });
+    btnSun.addEventListener('click', () => {
+        removeAndAdd(btnMoon,btnSun)
+        const darkTheme = document.querySelector('#dark-theme')
+        darkTheme.remove();
+    });
+}
+selectThem();
